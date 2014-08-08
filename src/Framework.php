206daffd69f4ffbcd3084019d0dffa71173c1707
Atif Ghaffar <atif.ghaffar@gmail.com>
2014-08-08 11:46:14 +0200

    Adding license, composer and src

---
+++ src/Framework.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of allinora/simple/framework.
+ *
+ * (c) Atif Ghaffar <atif.ghaffar@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Allinora\Simple;
+
+/**
+ * Framework
+ *
+ * @package   com.allinora.simmple.framework
+ * @author    Atif Ghaffar <atif.ghaffar@gmail.com>
+ * @copyright 2012-2014 Atif Ghaffar
+ * @license   http://www.opensource.org/licenses/MIT The MIT License
+ */
+class Framework {
+	public function __construct(){
+		print "Hello world";
+	}
+}
